979bb2a85c8cf91510f2583a95e605b9ab1a7609
funwangsir <wangs5957@foxmail.com>
2020-11-18 17:48:55 +0800

    1.colors.xml添加了一些自定义的颜色库 2.确定了图片和资源文件存放位置   图片放在drawable-mdpi   背景等资源文件存放在drawable 3.完成了底部导航栏的基本结构，但点击底部导航栏的跳转还未完成

---
+++ app/src/main/java/com/wechat/Frame/MyFragment.java
@@ -0,0 +1,27 @@
+package com.wechat.Frame;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.wechat.R;
+
+public class MyFragment extends Fragment {
+
+    private String content;
+    public MyFragment(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.frame_content,container,false);
+        TextView txt_content = (TextView) view.findViewById(R.id.txt_content);
+        txt_content.setText(content);
+        return view;
+    }
+}
--- app/src/main/java/com/wechat/MainActivity.java
+++ app/src/main/java/com/wechat/MainActivity.java
@@ -3,12 +3,20 @@ package com.wechat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import com.wechat.Frame.MyFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
 }
