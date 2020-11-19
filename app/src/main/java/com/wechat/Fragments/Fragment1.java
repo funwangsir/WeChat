d7de8fa6bc48cba51b3ee86733a19ebf973ea828
funwangsir <wangs5957@foxmail.com>
2020-11-19 11:18:15 +0800

    优化项目结构

---
+++ app/src/main/java/com/wechat/Fragments/Fragment1.java
@@ -0,0 +1,16 @@
+package com.wechat.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import com.wechat.R;
+
+public class Fragment1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.menu_fragment_1,container,false);
+        return view;
+    }
+}
